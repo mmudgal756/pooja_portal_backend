f7e871dbe8ffcc18c910c89b22d34ca47e152d65
mmudgal756 <mmudgal756@gmail.com>
2025-09-03 16:20:11 +0000

    gg

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,12 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+}));
 app.use(express.json());
 
 // Routes
