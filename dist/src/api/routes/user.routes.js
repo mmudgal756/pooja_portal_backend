d27a0055923dd0e233c4f45e478f084ce43fca09
mmudgal756 <mmudgal756@gmail.com>
2025-09-04 05:23:58 +0000

    f

--- dist/src/api/routes/user.routes.js
+++ dist/src/api/routes/user.routes.js
@@ -78,5 +78,41 @@ router.post('/register', user_validator_1.registerValidator, user_controller_1.r
  *         description: Invalid credentials
  */
 router.post('/login', user_validator_1.loginValidator, user_controller_1.login);
+/**
+ * @swagger
+ * /api/users/{id}/role:
+ *   put:
+ *     summary: Update a user's role
+ *     tags: [Users]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The ID of the user to update
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               role:
+ *                 type: string
+ *                 enum: [Admin, Vendor, Customer]
+ *             required:
+ *               - role
+ *     responses:
+ *       200:
+ *         description: User role updated successfully
+ *       400:
+ *         description: Bad request
+ *       404:
+ *         description: User not found
+ *       500:
+ *         description: Server error
+ */
+router.put('/:id/role', user_controller_1.updateUserRole);
 exports.default = router;
 //# sourceMappingURL=user.routes.js.map
